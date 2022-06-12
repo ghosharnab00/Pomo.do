11fad557eb2ed00eeab83f09b7ac79e01c2cd402
Arnab Ghosh <ghosharnab00@gmail.com>
2022-06-12 16:21:03 +0530

    2nd attempt.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -56,7 +56,26 @@ export default function Home() {
     let settingcontext = useContext(SettingContext);
     const [toggleState, setToggleState] = useState(false);
     const [input, setInput] = useState("")
-    const [todolist, setTodolist] = useState(JSON.parse(localStorage.getItem('todos')))
+
+
+    let settodolocalstorage = ()=>{
+      if (localStorage.length===0){
+        localStorage.setItem('todos', JSON.stringify([]));
+        localStorage.setItem('dones', JSON.stringify([]));
+        return JSON.parse(localStorage.getItem('todos'))
+      }
+      else{
+        return JSON.parse(localStorage.getItem('todos'));
+      }
+
+    }
+
+
+
+
+
+
+    const [todolist, setTodolist] = useState(settodolocalstorage)
     const [donelist, setDonelist] = useState(JSON.parse(localStorage.getItem('dones')))
 
     // JSON.parse(localStorage.getItem('todos'))
