88f0d6ac39bd058dfd681f22d3d336673e814e81
Arnab g <ag@Arnabs-MacBook-Air.local>
2022-07-17 00:47:34 +0530

    Add State

--- client/src/components/state/state.jsx
+++ client/src/components/state/state.jsx
@@ -1,10 +1,106 @@
-import React from 'react'
-import './state.css';
+import React from "react";
+import { Box, Container, Grid } from '@mui/material';
+import { Budget } from './dashboard/budget';
+import { LatestOrders } from './dashboard/latest-orders';
+import { LatestProducts } from './dashboard/latest-products';
+import { Sales } from './dashboard/sales';
+import { TasksProgress } from './dashboard/tasks-progress';
+import { TotalCustomers } from './dashboard/total-customers';
+import { TotalProfit } from './dashboard/total-profit';
+import { TrafficByDevice } from './dashboard/traffic-by-device';
 
-export default function States() {
-  return (
-    <div className='state'>
-      Hi This is State
-    </div>
-  )
-}
+const State = () => (
+  <>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalProfit sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <Sales />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <TrafficByDevice sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <LatestProducts sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <LatestOrders />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
+
+
+
+export default State;
