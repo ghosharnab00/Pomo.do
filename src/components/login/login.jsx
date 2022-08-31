1ac66b96d68d69ef884178446b08f10c27fe0547
Arnab g <ag@Arnabs-MacBook-Air.local>
2022-08-31 16:18:41 +0530

    Added Use As. A Guest Mode

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useContext} from 'react'
-import {  Button, Typography, Modal, Box } from "@mui/material";
+import {  Button, Typography, Modal, Box, Divider } from "@mui/material";
 import GoogleIcon from '@mui/icons-material/Google';
 import SettingContext from '../settings/settingcontext';
 import {btnstyle, iconstyle,modelstyle, messageStyle } from "./loginstyle";
@@ -31,6 +31,9 @@ const settingcontext = useContext(SettingContext);
         <Box sx={modelstyle}>
    <Button type='submit' color='primary' variant="outlined"  style={btnstyle} startIcon={<GoogleIcon style={iconstyle}/>} onClick={googleAuth} > Sign in With Google Account</Button>
    <Typography style={messageStyle}>Log in to start using the tool now! <span role="img" aria-label='emoji'>😃</span></Typography>
+   <Divider/>
+   <Button color='secondary'  style={btnstyle} onClick={()=>setOpen(false)} > Use as A Guest </Button>
+   <Typography color="red" style={messageStyle}>You can't use the ToDo and Stats if you use Guest Mode</Typography>
    </Box>
    </Modal>
         </div>
