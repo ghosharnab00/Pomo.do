f8016a125450ed67288d7f83fbcd47672d2f32eb
Arnab g <ag@Arnabs-MacBook-Air.local>
2022-08-31 15:50:05 +0530

    Update axiosConfig.jsx

--- src/data/axiosConfig.jsx
+++ src/data/axiosConfig.jsx
@@ -1,3 +1,3 @@
-export const api = 'https://bwop6isy8k.execute-api.ap-south-1.amazonaws.com/dev/api'
-// export const api = 'https://pomo-do.herokuapp.com/api'
+// export const api = 'https://bwop6isy8k.execute-api.ap-south-1.amazonaws.com/dev/api'
+export const api = 'https://pomo-do.herokuapp.com/api'
 
